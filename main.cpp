131a210432f5840ad1b70fddfd6278f5df9a2907
Yash <koenigseggism.com>
2024-12-25 00:02:05 +0530

    adding second set

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-#include <iostream>
+// double hit_sphere(const point3& center, double radius, const ray& r){
+//     vec3 oc = center - r.origin();
+//     auto a = r.direction().length_squared();
+//     auto h = dot(r.direction(), oc);
+//     auto c = oc.length_squared() - radius*radius;
+//     auto discr= h*h - a*c;
 
-bool hit_sphere(const point3& center, double radius, const ray& r){
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
+//     if (discr < 0){
+//         return -1.0;
+//     }else{
+//         return (h - std::sqrt(discr))/a;
+//     }
+// }
 
-color ray_color(const ray& r){
-    if(hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1,0,0);
+color ray_color(const ray& r, const hittable& world){
+    hit_record rec;
+    if(world.hit(r, 0, infinity, rec)){
+        return 0.5*color(rec.normal + color(1,1,1));
+    }
+    
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -31,6 +38,11 @@ int main(){
     /* image height must be atleast 1 */
     int image_height = int (image_width / aspect_ratio);
     image_height = (image_height < 1 ? 1 : image_height);
+
+    // world
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5, -1), 100));
 
     // camera
     auto focal_length = 1.0;
@@ -67,8 +79,9 @@ int main(){
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            // color pixel_color = ray_color(r);
             // auto pixel_color = color(double(i)/(image_width - 1), double(j)/(image_height - 1), 0);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         /*    auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
---
+++ sphere.h
@@ -0,0 +1,38 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+
+class sphere : public hittable{
+    public:
+        sphere(const point3& center, double radius): center(center), radius(std::fmax(0, radius)){}
+
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override{
+            vec3 oc = center - r.origin();
+            auto a = r.direction().length_squared();
+            auto h = dot(r.direction(), oc);
+            auto c = oc.length_squared() - radius*radius;
+            
+            auto discr = h*h - a*c;
+            if(discr < 0)
+                return false;
+
+            auto sqrtd = std::sqrt(discr);
+
+            auto root = (h - sqrtd) / a;
+            if(root <= ray_tmin || ray_tmax <= root){
+                return false;
+
+            }
+            rec.t = root;
+            rec.p = r.at(rec.t);
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
+            return true;
+        }
+        private:
+            point3 center;
+            double radius;
+};
+
+#endif
